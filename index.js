6ca9256d00ccfd4a395348e3b3656a851c54881c
Praniyal Thapa <praniyalthapa100@gmail.com>
2024-04-29 21:24:14 +0545

    basic package definition creaked

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const grpc=require('@grpc/grpc-js');
-const todoProto=grpc.load('todo.proto');
-
+//const todoProto=grpc.load('todo.proto'); //loaded the proto
+const protoLoader = require('@grpc/proto-loader');
+const packageDefinition=protoLoader.loadSync('./todo.proto',{
+    keepCase: true, 
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+});
 
 
 const server=new grpc.Server();
@@ -16,13 +23,14 @@ const todos=[
 ];
 server.addService(todoProto.TodoService.service,{ //using todo proto u can use its services
     ListTodo:(call,callback)=>{        
-        callback(null,todos);      //sendinng data to client side
+        callback(null,todos);      //sendinng data to client side where erro is null
         
     },
     CreateTodo :(call,callback)=>{
          let newTodo=call.request;
          todos.push(newTodo);
-         callback(null,newTodo);  //sending created object back to the client
+        callback(null,todos); //sending created object back to the client
+        
     },
     GetTodo:(call,callback)=>{
       let incommingRequest=call.request; //this will give you whole TodoRequest object
@@ -41,3 +49,6 @@ server.addService(todoProto.TodoService.service,{ //using todo proto u can use i
 
 
 }); 
+server.bind('127.0.0.1:50051',grpc.ServerCredentials.createInsecure()); //binding our server with ip 
+console.log('server started');
+server.start();
